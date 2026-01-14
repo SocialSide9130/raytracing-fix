7e9e34fedf0176b3f9278f3957d98472d2611e1a
Sogo SATO <s216458w@st.go.tuat.ac.jp>
2026-01-14 18:38:59 +0900

    https://github.com/tttmmmyyyy/fixlang/issues/29 のため -g を削除

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker run --rm -it -v "$PWD":/work -w /work pt9999/fixlang fix build -o main --backtrace -g -f *.fix
+docker run --rm -it -v "$PWD":/work -w /work pt9999/fixlang fix build -o main --backtrace -f *.fix
 if [ "$?" -eq "0" ] ; then
     ./main > ./image.ppm
     rm ./main
